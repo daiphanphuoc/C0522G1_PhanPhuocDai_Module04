bda0a897910c843a051994a1d7b2902f4568c25b
daiphanphuoc <daiphanphuoc@gmail.com>
2022-09-29 23:00:42 +0700

    jquery & ajax

--- ss12_ajax/exercise/phone/phone_manager/src/main/java/daiphan/exercise/controller/SmartphoneController.java
+++ ss12_ajax/exercise/phone/phone_manager/src/main/java/daiphan/exercise/controller/SmartphoneController.java
@@ -2,6 +2,7 @@ package daiphan.exercise.controller;
 
 import daiphan.exercise.model.Smartphone;
 import daiphan.exercise.service.ISmartphoneService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -33,6 +34,15 @@ public class SmartphoneController {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Smartphone> getSmartphone(@PathVariable int id) {
+        Optional<Smartphone> smartphone = smartphoneService.findById(id);
+        if (smartphone.isPresent()) {
+            return new ResponseEntity<>(smartphone.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
     @DeleteMapping("/{id}")
     public ResponseEntity<Smartphone> deleteSmartphone(@PathVariable int id) {
         Optional<Smartphone> smartphoneOptional = smartphoneService.findById(id);
@@ -41,5 +51,16 @@ public class SmartphoneController {
         }
         smartphoneService.remove(id);
         return new ResponseEntity<>(smartphoneOptional.get(), HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Smartphone> putSmartphone(@PathVariable int id, @RequestBody Smartphone smartphone) {
+        Optional<Smartphone> smartphoneOptional = smartphoneService.findById(id);
+        if (!smartphoneOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        System.out.println(smartphone);
+        smartphoneService.save(smartphone);
+        return new ResponseEntity<>(smartphone, HttpStatus.NO_CONTENT);
     }
 }
